9f10afca09fb40b4313a0c968419eb09182d9afc
Daniel <danielga@outlook.pt>
2018-08-23 23:35:14 +0100

    Bumped revision number

--- source/main.cpp
+++ source/main.cpp
@@ -11,11 +11,11 @@ GMOD_MODULE_OPEN( )
 {
 	LUA->CreateTable( );
 
-	LUA->PushString( "luaerror 1.5.0" );
+	LUA->PushString( "luaerror 1.5.1" );
 	LUA->SetField( -2, "Version" );
 
 	// version num follows LuaJIT style, xxyyzz
-	LUA->PushNumber( 10500 );
+	LUA->PushNumber( 10501 );
 	LUA->SetField( -2, "VersionNum" );
 
 #if defined LUAERROR_SERVER
