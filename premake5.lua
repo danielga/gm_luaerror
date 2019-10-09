9b45522a2bcbd358edd9fe1edd2c8e58170fa6bf
Daniel <danielga@outlook.pt>
2019-10-09 01:31:14 +0100

    Update project generator
    Update license copyright
    Update README

--- premake5.lua
+++ premake5.lua
@@ -4,22 +4,20 @@ newoption({
 	value = "path to garrysmod_common directory"
 })
 
-local gmcommon = _OPTIONS.gmcommon or os.getenv("GARRYSMOD_COMMON")
-if gmcommon == nil then
-	error("you didn't provide a path to your garrysmod_common (https://github.com/danielga/garrysmod_common) directory")
-end
-
-include(gmcommon)
+local gmcommon = assert(_OPTIONS.gmcommon or os.getenv("GARRYSMOD_COMMON"),
+	"you didn't provide a path to your garrysmod_common (https://github.com/danielga/garrysmod_common) directory")
+include(path.join(gmcommon, "generator.v2.lua"))
 
 CreateWorkspace({name = "luaerror", abi_compatible = true})
 	CreateProject({serverside = true, manual_files = true})
 		files({
-			"../source/main.cpp",
-			"../source/server.cpp",
-			"../source/server.hpp",
-			"../source/shared.cpp",
-			"../source/shared.hpp"
+			"source/main.cpp",
+			"source/server.cpp",
+			"source/server.hpp",
+			"source/shared.cpp",
+			"source/shared.hpp"
 		})
+		IncludeLuaShared()
 		IncludeSDKCommon()
 		IncludeSDKTier0()
 		IncludeSDKTier1()
@@ -28,10 +26,11 @@ CreateWorkspace({name = "luaerror", abi_compatible = true})
 
 	CreateProject({serverside = false, manual_files = true})
 		files({
-			"../source/main.cpp",
-			"../source/shared.cpp",
-			"../source/shared.hpp"
+			"source/main.cpp",
+			"source/shared.cpp",
+			"source/shared.hpp"
 		})
+		IncludeLuaShared()
 		IncludeSDKCommon()
 		IncludeSDKTier0()
 		IncludeSDKTier1()
