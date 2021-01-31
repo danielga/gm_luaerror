2a1d72069748080d2c309f3e3629f7b37e6a79d6
Daniel <danielga@outlook.pt>
2021-01-31 21:52:03 +0000

    Updated project generator inclusion

--- premake5.lua
+++ premake5.lua
@@ -1,12 +1,13 @@
+PROJECT_GENERATOR_VERSION = 2
+
 newoption({
 	trigger = "gmcommon",
 	description = "Sets the path to the garrysmod_common (https://github.com/danielga/garrysmod_common) directory",
 	value = "path to garrysmod_common directory"
 })
 
-local gmcommon = assert(_OPTIONS.gmcommon or os.getenv("GARRYSMOD_COMMON"),
-	"you didn't provide a path to your garrysmod_common (https://github.com/danielga/garrysmod_common) directory")
-include(gmcommon .. "/generator.v2.lua")
+include(assert(_OPTIONS.gmcommon or os.getenv("GARRYSMOD_COMMON"),
+	"you didn't provide a path to your garrysmod_common (https://github.com/danielga/garrysmod_common) directory"))
 
 CreateWorkspace({name = "luaerror", abi_compatible = true})
 	CreateProject({serverside = true, manual_files = true})
