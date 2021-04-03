1cbffa73458f39408572b2fc7f7ae34cc31d5854
Daniel <danielga@outlook.pt>
2021-04-03 22:47:46 +0100

    Bumped revision number for garrysmod_common fixes

--- source/main.cpp
+++ source/main.cpp
@@ -12,11 +12,11 @@ GMOD_MODULE_OPEN( )
 {
 	LUA->CreateTable( );
 
-	LUA->PushString( "luaerror 1.5.5" );
+	LUA->PushString( "luaerror 1.5.6" );
 	LUA->SetField( -2, "Version" );
 
 	// version num follows LuaJIT style, xxyyzz
-	LUA->PushNumber( 10505 );
+	LUA->PushNumber( 10506 );
 	LUA->SetField( -2, "VersionNum" );
 
 #if defined LUAERROR_SERVER
